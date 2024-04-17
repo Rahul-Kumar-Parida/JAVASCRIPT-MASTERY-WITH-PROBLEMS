d594344cfd6d3201e03423a0e5d3d5e05c578f28
Rahul-Kumar-Parida <rahulkumarparida56@gmail.com>
2024-04-17 19:16:23 +0530

    add new

---
+++ Day07/index.js
@@ -0,0 +1,16 @@
+//Find a Maximum Number In an Array....
+
+//method-1
+const MaxiMum = (arg) => {
+  return Math.max(...arg);
+};
+
+console.log(MaxiMum([3, 4, 5, 16, 7, 8, 9, 2]));
+
+const arr = [3, 4, 56, 7, 8, 54];
+console.log(arr); //[ 3, 4, 56, 7, 8, 54 ]
+console.log(...arr); //3 4 56 7 8 54
+
+
+//method 2;
+
